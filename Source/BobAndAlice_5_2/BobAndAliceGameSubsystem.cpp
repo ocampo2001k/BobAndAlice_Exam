df8d11397b0c6fdc711b1fb0250a1d9a2fc905b2
ocampo2001k <carlosocampocantillo@gmail.com>
2023-12-14 21:26:33 -0500

    Final_Exam_carlos_ocampo
    issue 3 is half done.

--- Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -1,8 +1,36 @@
 ﻿#include "BobAndAliceGameSubsystem.h"
 
-void UBobAndAliceGameSubsystem::Initialize(
-    FSubsystemCollectionBase &Collection) {
-  Super::Initialize(Collection);
+// Constructor
+UBobAndAliceGameSubsystem::UBobAndAliceGameSubsystem ()
+{
+	// Set default values for the current time
+	TimeRemaining = 45;
 }
 
-void UBobAndAliceGameSubsystem::Deinitialize() { Super::Deinitialize(); }
+// Initialize the subsystem
+void
+UBobAndAliceGameSubsystem::Initialize (FSubsystemCollectionBase &Collection)
+{
+	Super::Initialize (Collection);
+}
+
+// Deinitialize the subsystem
+void
+UBobAndAliceGameSubsystem::Deinitialize ()
+{
+	Super::Deinitialize ();
+}
+
+// Function to set the current time
+void
+UBobAndAliceGameSubsystem::SetCurrentTime (float NewTime)
+{
+	TimeRemaining = NewTime;
+}
+
+// Function to get the current time
+float
+UBobAndAliceGameSubsystem::GetCurrentTime () const
+{
+	return TimeRemaining;
+}
--- Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -1,19 +1,37 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
-
 #include "BobAndAliceGameSubsystem.generated.h"
 
-UCLASS()
+/**
+ *
+ */
+UCLASS ()
 class BOBANDALICE_5_2_API UBobAndAliceGameSubsystem : public UGameInstanceSubsystem
 {
-  GENERATED_BODY()
+	GENERATED_BODY ()
 
 public:
-  virtual void Initialize(FSubsystemCollectionBase &Collection) override;
+	// Constructor
+	UBobAndAliceGameSubsystem ();
 
-  virtual void Deinitialize() override;
+	// Initialize the subsystem
+	virtual void Initialize (FSubsystemCollectionBase &Collection) override;
 
+	// Deinitialize the subsystem
+	virtual void Deinitialize () override;
+
+	// Function to set the current times
+	UFUNCTION (BlueprintCallable)
+	void SetCurrentTime (float NewTime);
+
+	// Function to get the current time
+	UFUNCTION (BlueprintCallable)
+	float GetCurrentTime () const;
+
+private:
+	// Variable to store the current time
+	UPROPERTY (VisibleAnywhere, BlueprintReadOnly, Category = "Time",
+	           meta = (AllowPrivateAccess = "true"))
+	float TimeRemaining;
 };
